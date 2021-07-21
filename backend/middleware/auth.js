61c7b27783a50491cc0a97e7bd97b455a06ad053
y4wee <quentin.jambert@gmail.com>
2021-07-21 18:01:40 +0200

    auth autorisation

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'un_probleme_sans_solution_est_un_probleme_mal_pose');
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'Invalid user ID';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error('Invalid request!')
+    });
+  }
+};

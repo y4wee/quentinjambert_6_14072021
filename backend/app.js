3a9c3895fb0a6d24ec65443fadd48fc9ce2e3b98
y4wee <quentin.jambert@gmail.com>
2021-08-01 12:59:48 +0200

    install sanitize

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 //importation des plugins
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoSanitize = require('express-mongo-sanitize');
 const mongoose = require('mongoose');
 const path = require('path');
 
@@ -12,22 +13,25 @@ const sauceRoutes = require('./routes/sauce');
 const app = express();
 
 // adresse de connexion pour la bdd MongoDB
-mongoose.connect('mongodb+srv://y4wee:iAzqDkU0yqGA3hBd@clustertest.4jtzg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://user:v7E3JthKEdU4bbzR@clustertest.4jtzg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // mise en place CORS
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 // parser automatiquement le coprs de la reponse
 app.use(bodyParser.json());
+
+// supprime les clé json $
+app.use(mongoSanitize());
 
 
 // declaration des routes de l'api

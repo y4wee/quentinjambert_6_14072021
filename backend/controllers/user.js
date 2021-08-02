89b68620f51aae1ef4cb667037171a7c8f348252
y4wee <quentin.jambert@gmail.com>
2021-08-02 16:47:38 +0200

    comentaire

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -11,7 +11,7 @@ exports.signup = (req, res, next) => {
           email: req.body.email,
           password: hash
         });
-        user.save() // sauvegarde les les informations utilisateur
+        user.save() // sauvegarde les informations utilisateur
           .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
           .catch(error => res.status(400).json({ error }));
       })

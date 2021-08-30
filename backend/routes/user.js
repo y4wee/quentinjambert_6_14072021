88816dd4345e404fa2afe97c21cf6f6cc548e2d7
y4wee <quentin.jambert@gmail.com>
2021-08-30 17:19:05 +0200

    ratelimiter pour les connexions

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,11 +2,21 @@
 const express = require('express');
 const router = express.Router();
 
+// utilisé pour empêcher les adresses IP de faire des demandes répétées aux points de terminaison de l'API
+const rateLimit = require('express-rate-limit');
+
 // importation controller
 const userCtrl = require('../controllers/user');
 
+// rateLimit pour les connexions
+const connexionLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 5, // bloque apres 5 requetes
+    skipSuccessfulRequests: true,
+  });
+
 //declare les routes possible vers /api/auth
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login',connexionLimiter, userCtrl.login);
 
 module.exports = router;
